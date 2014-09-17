cfb35477459806f47a198b34094825979da43487
Chiroc <470597142@qq.com>
2014-09-17 12:43:35 +0800

    Code optimization and extensions.

--- jquery-fastsearch-0.1.js
+++ jquery-fastsearch-0.1.js
@@ -3,37 +3,56 @@
  * @project: http://git.oschina.net/470597142/jquery-fastsearch
  * @auth: Chiroc(470597142@qq.com)
  * @dependence jQuery
- * @param $
- */
+ * @usage:
+ * var target = $('#scope').find('td'); //The target for searching. 
+ * //$('#demo-4') is input object.
+ * $('#demo-4').fastSearch(target, {
+ *      autoToggle: true, //Toggle show/hide DOM node when searching.
+ *      caseSensitive: true, //Case sensitive.
+ *      color: 'white', //font color for results.
+ *      background: '#00f' //background color for results.
+ *      parent: true //when want to hide it's parent node when toggle
+ *      afterClear: function (self) {
+ *          // afterClear: event after clear search results.
+ *      },
+ *      beforeSearch: function (self, keyWords) {
+ *         // beforeSearch: event before search started.
+ *      },
+ *      afterSearch: function (self, keyWords, result, count) {
+ *         // afterSearch: event after search completed.
+ *      }
+ * });
+ **/
 (function ($) {
     $.fn.fastSearch = function (target, options) {
         var self = $(this),
-            setting = {
-                autoToggle: true,
-                caseSensitive: false,
-                color: '#000',
-                background: '#FF9632',
-                afterClear: function (self) {
-                    if(this.autoToggle){
-                        target.show();
-                    }
-                },
-                beforeSearch: function (self, keyWords) {
-                    if(this.autoToggle){
-                        target.hide();
-                    }
-                },
-                afterSearch: function (self, keyWords, result, count) {
-                    if(this.autoToggle){
-                        result.show();
-                    }
-                }
-            },
             result = [],
             count = 0,
             flags = 'gi',
             style = '',
-            KEY_CODE_ESC = 27;
+            KEY_CODE_ESC = 27,
+            setting = {
+                parent: false,
+                autoToggle: false,
+                caseSensitive: false,
+                color: '#000',
+                background: '#FF9632',
+                afterClear: function (self) {
+                    if (this.autoToggle) {
+                        this.parent ? target.parent().show() : target.show();
+                    }
+                },
+                beforeSearch: function (self, keyWords) {
+                    if (this.autoToggle) {
+                        this.parent ? target.parent().hide() : target.hide();
+                    }
+                },
+                afterSearch: function (self, keyWords, result, count) {
+                    if (this.autoToggle) {
+                        this.parent ? result.parent().show() : result.show();
+                    }
+                }
+            };
 
         setting = $.extend(setting, options);
         style = ['color:', setting.color, ';background:', setting.background].join('');
@@ -41,8 +60,8 @@
 
         if (setting.caseSensitive) {
             flags = 'g';
-        }else{
-            $.expr[':'].contains = function (a, i, m) {
+        } else {
+            $.expr[':'].contains_ext = function (a, i, m) {
                 return $(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
             };
         }
@@ -68,7 +87,7 @@
                 }
                 setting.beforeSearch(self, keyWords);
 
-                result = target.filter(':contains("' + keyWords + '")');
+                result = target.filter(':contains_ext("' + keyWords + '")').not('.ignore');
                 count = result.length;
                 _.clearHighlight();
 
